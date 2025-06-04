8f8573fcd305a1244e1fe6a9fb3504d7ce1e5284
nevsetakpohano <aya101318@gmail.com>
2025-06-04 22:41:08 +0300

    Tested alpine

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
-FROM python:3.10-slim
+FROM python:3.10-alpine
 WORKDIR /app
-COPY . .
+RUN apk add --no-cache gcc musl-dev
+COPY requirements.txt .
 RUN pip install --no-cache-dir -r requirements.txt
-
+COPY . .
 CMD ["uvicorn", "spaceship.main:app", "--host=0.0.0.0", "--port=8080"]
--- spaceship/routers/api.py
+++ spaceship/routers/api.py
@@ -1,3 +1,4 @@
+import numpy as np
 from fastapi import APIRouter
 
 router = APIRouter()
@@ -6,3 +7,14 @@ router = APIRouter()
 @router.get('')
 def hello_world() -> dict:
     return {'msg': 'Hello, World!'}
+
+@router.get("/multiply-matrices")
+def multiply_matrices():
+    matrix_a = np.random.rand(10, 10)
+    matrix_b = np.random.rand(10, 10)
+    product = np.dot(matrix_a, matrix_b)
+    return {
+        "matrix_a": matrix_a.tolist(),
+        "matrix_b": matrix_b.tolist(),
+        "product": product.tolist()
+    }
